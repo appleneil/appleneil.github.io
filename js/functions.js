2ecc4aa0713d4357182e19cc50268df2728e0eb3
Neil Appleton <neil.appleton@interaction-healthcare.com>
2017-11-03 15:02:55 +0100

    added animation home + logo

--- js/functions.js
+++ js/functions.js
@@ -4,4 +4,28 @@ $(function(){
 		$(this).parents('.site-footer').toggleClass('is-visible');
 	});
 
+	setTimeout(function(){ 
+
+		var $header = document.querySelector('.site-header'),
+			$svg = document.querySelector('.shape'),
+			svgPath = $svg.querySelector('path');
+		
+		anime({
+			targets: $header,
+			translateY: {
+				value: '-200vh', 
+				duration: 2000,
+				easing: 'easeInOutQuad'
+			}
+		});
+
+		anime({
+			targets: svgPath,
+			duration: 1200, 
+			easing: 'linear',
+			d: svgPath.getAttribute('pathdata:id')
+		});
+
+	}, 2000);
+
 });
